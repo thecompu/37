113da532c1ea1e0d977b203c30d244f3fb56bca2
tcv <mikew@thecomputervalet.com>
2013-07-19 16:42:49 -0500

    hover and add/remove classes

--- scripts/application.js
+++ scripts/application.js
@@ -1,8 +1,10 @@
 
 $(document).ready(function() {
-	$('.div_BC_Box').on('mouseenter', function() {
-	$('div.div_BC_SwapZ').toggleClass('div_BC_Swap');
-	})
+	$('.div_BC_Box').hover(function() {
+	$('div.div_BC_SwapZ').addClass('div_BC_Swap');
+	}, function() {
+		$('div.div_BC_SwapZ').removeClass('div_BC_Swap');
+	});
 })
 
 /*$('.div_BC_Box').on('mouseenter', function(){
