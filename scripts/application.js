bfdbf0e1fda075c92060c82ac9f562eee206f409
tcv <mikew@thecomputervalet.com>
2013-07-19 10:05:18 -0500

    make hover toggleclass

--- scripts/application.js
+++ scripts/application.js
@@ -1,7 +1,9 @@
 
 $(document).ready(function() {
+	$('.div_BC_Box').on('mouseenter', function() {
 	$('div.div_BC_SwapZ').toggleClass('div_BC_Swap');
 	})
+})
 
 /*$('.div_BC_Box').on('mouseenter', function(){
 		$(this).find('.div_BC_BoxZ').toggleClass('div_BC_Box');
